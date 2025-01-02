10485d8118a266ba293c6d6ad714d38963b154a6
Edouard DUCLOY <edouard.ducloy@humanisys.fr>
2025-01-02 11:13:45 +0100

    Display image with mark of the position to the text to labeled and store the label in a JSON dataset

--- labelizer.py
+++ labelizer.py
@@ -1,18 +1,23 @@
 import tkinter as tk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import json
 
-
-def SignalSource(images, positions):
+def SignalSource(images, positions, label):
     for img, pos in zip(images, positions):
-        yield (img, pos)
-        input("Next")
+        l = []
+        for p in pos:
+            yield (img, p)
+            l.append(input("Label: "))
+        label.append(l)
 
 
 class OCRLabelizer(tk.Tk):
     def __init__(self,images, positions,  *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.signal = SignalSource(images, positions)
+        self._label = []
+        self._positions = positions
+        self.signal = SignalSource(images, positions, self._label)
 
         # Create a plot that can be embedded in a tkinter window.
         self.figure = Figure(figsize=(6, 4))
@@ -30,7 +35,12 @@ class OCRLabelizer(tk.Tk):
             self.plt.clear()
             self.plt.margins(x=0)
             self.plt.imshow(img)
+            self.plt.scatter(pos[0], pos[1], s=1000, c='red', marker='x', clip_on=False)
             self.figure.canvas.draw()
             self.after(100, self.update_plot)
         except StopIteration:
             print("All picture are labelled")
+            self.quit()
+            data = {l:p for label, position in zip(self._label, self._positions) for l, p in zip(label, position)}
+            with open("dataset.json", "w") as f:
+                json.dump(data, f)
